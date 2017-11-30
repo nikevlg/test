f18f16fdf1b8b2f8aeea384c1d586fe3bbdb309f
Tatiana Potapova <tatiana_potapova@inbox.ru>
2017-11-30 11:25:24 +0300

    Сделана логика для форм "вопросы" и "заказать звонок"
    Signed-off-by: Tatiana Potapova <tatiana_potapova@inbox.ru>

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -93,7 +93,7 @@ var calculatorData = {
     var price = pricePerMonth * monthsAmount;
     var economy = this.taxPerMonth[this.selectedTariff][0] * monthsAmount - price;
     var monthEnd = this.monthEnds[this.selectedMonthIndex];
-    document.getElementsByClassName('js-price-per-month')[0].innerHTML = 'Цена за ' + monthsAmount + ' ' + monthEnd + ': ' + pricePerMonth.toLocaleString() + ' руб.';
+    document.getElementsByClassName('js-price-per-month')[0].innerHTML = 'Цена за 1 месяц: ' + pricePerMonth.toLocaleString() + ' руб.';
     if (economy > 0 || this.selectedTariff == 0) {
       document.getElementsByClassName('js-economy-big')[0].innerHTML = 'Экономия: ' + economy.toLocaleString() + ' руб.';
       document.getElementsByClassName('js-economy-big')[0].style.display = 'block';
@@ -109,8 +109,188 @@ var calculatorData = {
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   calculatorData.init();
+  initCallbackPopup();
+  initForms();
 });
 // End calculator
+
+// Callback popup
+function initCallbackPopup() {
+  var toggleElement = $('#js-callback-toggle');
+  var callbackPopup = $('#js-form-callback');
+  toggleElement.click(function(event) {
+    event.preventDefault();
+    callbackPopup.toggleClass('navigate__callback--active');    
+  });
+  $('main').click(function(){
+    callbackPopup.removeClass('navigate__callback--active');
+  });
+  $('.footer').click(function(){
+    callbackPopup.removeClass('navigate__callback--active');
+  }); 
+}
+// End callback popup
+
+// Forms
+function initForms() {
+  var emailTest = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var phoneTest = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+  var loginTest = /^[a-zA-Z0-9_\-@\+\.]{3,30}$/;
+  var nameTest = /^[a-zA-Zа-яА-ЯёЁ\-\s]{2,50}$/;
+
+  $('.js-remove-error').click(function(e){
+    e.preventDefault();
+    $(this).removeClass('input-error');
+    $('.error-message').removeClass('error-message');
+    $('.success-message').removeClass('success-message');
+  })
+
+  $('#js-callback-name').click(function(e){
+    e.preventDefault();
+    $('#js-callback-name-error').text('');
+    $('#js-callback-success-error').text('');
+  });
+
+  $('#js-callback-email').click(function(e){
+    e.preventDefault();
+    $('#js-callback-email-error').text('');
+    $('#js-callback-success-error').text('');
+  })
+
+  $('#js-callback-phone').click(function(e){
+    e.preventDefault();
+    $('#js-callback-phone-error').text('');
+    $('#js-callback-success-error').text('');
+  })
+
+  $('#js-form-callback').on('submit', function(event) {
+    event.preventDefault();
+    var name = $('#js-callback-name');
+    var email = $('#js-callback-email');
+    var phone = $('#js-callback-phone');
+    var message = $('#js-callback-success-error');
+    var allCorrect = true;
+    if (!nameTest.test(name.val())) {
+      allCorrect = false;
+      $('#js-callback-name-error').text('Введите имя');
+    }
+    else {
+      $('#js-callback-name-error').text('');
+    }
+    if (!emailTest.test(email.val())) {
+      allCorrect = false;
+      $('#js-callback-email-error').text('Введите корректный e-mail');
+    }
+    else {
+      $('#js-callback-email-error').text('');
+    }
+    if (!phoneTest.test(phone.val())) {
+      allCorrect = false;
+      $('#js-callback-phone-error').text('Неверный формат');
+    }
+    else {
+      $('#js-callback-phone-error').text('');
+    }
+    var recaptcha = grecaptcha.getResponse(callbackRecaptcha);
+    if (recaptcha == '') {
+      allCorrect = false;
+      message.removeClass('success-message');
+      message.text('Пройдите проверку');
+    }
+    else {
+      message.text('');
+    }
+    if (allCorrect) {
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = true 
+      xhr.addEventListener('readystatechange', function() {
+        if (this.readyState == 4) {       
+          grecaptcha.reset(callbackRecaptcha);
+          if (status == 200) {          
+            name.val('');
+            email.val('');
+            phone.val('');
+            message.addClass('success-message');
+            message.innerText = 'Ваш звонок был успешно заказан';
+          }
+          else { 
+            message.removeClass('success-message');
+            message.innerText = 'Ошибка при заказе звонка';
+          }
+        }
+      });
+      xhr.open('POST', 'DYNAMIC_URL_REQUEST_API/api/promo/callback/order', true);
+      xhr.setRequestHeader('content-type', 'application/json'); 
+      xhr.send(JSON.stringify({
+        name: name.val(),
+        recaptcha: recaptcha,
+        email: email.val(),
+        phone: phone.val(),
+        timeTo: 23,
+        timeFrom: 0})
+      );
+    }
+  });
+
+  $('#js-form-questions').on('submit', function(event) {
+    event.preventDefault();
+    var name = $('#js-questions-name');
+    var email = $('#js-questions-email');
+    var text = $('#js-questions-text');
+    var message = $('#js-questions-success-error');
+    var allCorrect = true;
+    if (!nameTest.test(name.val())) {
+      allCorrect = false;
+      name.addClass('input-error');
+    }
+    if (!emailTest.test(email.val())) {
+      allCorrect = false;
+      email.addClass('input-error');
+    }
+    var recaptcha = grecaptcha.getResponse(questionsRecaptcha);
+    if (recaptcha == '') {
+      allCorrect = false;
+      message.removeClass('success-message');
+      message.addClass('error-message');
+      message.text('Пройдите проверку');
+    }
+    else {
+      message.text('');
+    }
+    if (allCorrect) {
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = true 
+      xhr.addEventListener('readystatechange', function() {
+        if (this.readyState == 4) {       
+          grecaptcha.reset(questionsRecaptcha);
+          if (status == 200) {          
+            name.val('');
+            email.val('');
+            phone.val('');
+            message.removeClass('error-message');
+            message.addClass('success-message');
+            message.innerText = 'Ваш звонок был успешно заказан';
+          }
+          else { 
+            message.removeClass('success-message');
+            message.addClass('error-message');
+            message.innerText = 'Ошибка при заказе звонка';
+          }
+        }
+      });
+      xhr.open('POST', 'DYNAMIC_URL_REQUEST_API/api/promo/feedback/send', true);
+      xhr.setRequestHeader('content-type', 'application/json'); 
+      xhr.send(JSON.stringify({
+        name: name.val(),
+        recaptcha: recaptcha,
+        email: email.val(),
+        message: text.val()})
+      );
+    }
+  });
+}
+// End forms
+
 
 //  Youtube player
 var firstScriptTag, onPlayerReady, player, tag;
